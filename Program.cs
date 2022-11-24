c07d4c0f09d7b59bad9993904ef82a898fad7688
Anastasia <kazantseva_nastya_95@mail.ru>
2022-11-25 01:36:30 +0500

    Array phrases

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 
 string[] array = { "Hello", "2", "world", ":-)", "go", "pen" };
 string[] array1 = new string[array.Length];
+
+int maxWordLength = 3;
+
+void ArrayPhrases(string[] arr, string[] arr1)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (arr[i].Length <= maxWordLength)
+    {
+      arr1[count] = arr[i];
+      count++;
+    }
+  }
+}
